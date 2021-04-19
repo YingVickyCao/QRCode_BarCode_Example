350a413c6ce8e89e4ee769ae91a334d14315d23f
YingVickyCao <ying.vicky.cao@outlook.com>
2021-04-19 13:50:50 +0800

    ..

--- JavaExample/src/main/java/com/hades/example/java/qrcode_barcode/QRCodeUtil.java
+++ JavaExample/src/main/java/com/hades/example/java/qrcode_barcode/QRCodeUtil.java
@@ -4,7 +4,10 @@ import com.google.zxing.*;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.DecoderResult;
+import com.google.zxing.common.DetectorResult;
 import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.multi.qrcode.detector.MultiDetector;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import com.hades.example.java.lib.FileUtils;
@@ -16,26 +19,45 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
-import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class QRCodeUtil {
     public static void generateQRCodeImage(String text, int width, int height, String filePath) throws WriterException, IOException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, createHints());
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, createEncodeHints());
         FileUtils.checkDestDirIsExist(filePath.substring(0, filePath.lastIndexOf("/")));
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", FileSystems.getDefault().getPath(filePath));
     }
 
-    private static Hashtable createHints() {
+    private static Map<EncodeHintType, ?> createEncodeHints() {
         // Create the ByteMatrix for the QR-Code that encodes the given String
-        Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<>();
+        Map<EncodeHintType, Object> hintMap = new HashMap<>();
+        // 设置容错率 L>M>Q>H 等级越高扫描时间越长,准确率越高
         hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        //设置字符集
+        hintMap.put(EncodeHintType.CHARACTER_SET, "utf-8");
+        //设置外边距
+        hintMap.put(EncodeHintType.MARGIN, 1);
+        return hintMap;
+    }
+
+    private static Map<DecodeHintType, ?> createDecodeHints() {
+        Map<DecodeHintType, Object> hintMap = new HashMap<>();
+        //设置字符集
+        hintMap.put(DecodeHintType.CHARACTER_SET, "utf-8");
+        hintMap.put(DecodeHintType.TRY_HARDER, true);
+        Collection<BarcodeFormat> barcodeFormats= new ArrayList<>();
+        barcodeFormats.add(BarcodeFormat.QR_CODE);
+        hintMap.put(DecodeHintType.POSSIBLE_FORMATS, barcodeFormats);
         return hintMap;
     }
 
     public static byte[] generateQRCodeByte(String text, int width, int height) throws WriterException, IOException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, createHints());
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, createEncodeHints());
         ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
         MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
         return pngOutputStream.toByteArray();
@@ -43,7 +65,7 @@ public class QRCodeUtil {
 
     public static void generateQRCodeImage2(String text, int width, int height, String filePath) throws WriterException, IOException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix byteMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, createHints());
+        BitMatrix byteMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, createEncodeHints());
 
         // Make the BufferedImage that are to hold the QRCode
         int matrixWidth = byteMatrix.getWidth();
@@ -51,10 +73,10 @@ public class QRCodeUtil {
         image.createGraphics();
 
         Graphics2D graphics = (Graphics2D) image.getGraphics();
-        graphics.setColor(Color.WHITE);
+        graphics.setColor(Color.GREEN);  // bg color
         graphics.fillRect(0, 0, matrixWidth, matrixWidth);
         // Paint and save the image using the ByteMatrix
-        graphics.setColor(Color.BLACK);
+        graphics.setColor(Color.RED);  // code color
 
         for (int i = 0; i < matrixWidth; i++) {
             for (int j = 0; j < matrixWidth; j++) {
@@ -74,11 +96,20 @@ public class QRCodeUtil {
         BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
 
         try {
-            Result result = new MultiFormatReader().decode(bitmap);
+            Result result = new MultiFormatReader().decode(bitmap, createDecodeHints());
             return result.getText();
         } catch (NotFoundException e) {
             System.out.println("There is no QR code in the image");
             return null;
         }
     }
+
+    public static void whetherImageContain2DBarCode(File qrCodeImage, Map<DecodeHintType, ?> hints) throws IOException, NotFoundException, FormatException {
+        BufferedImage bufferedImage = ImageIO.read(qrCodeImage);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        MultiDetector detector = new MultiDetector(bitmap.getBlackMatrix());
+        DetectorResult result = detector.detect(hints);
+    }
+
 }
--- JavaExample/src/main/java/com/hades/example/java/qrcode_barcode/Test.java
+++ JavaExample/src/main/java/com/hades/example/java/qrcode_barcode/Test.java
@@ -8,12 +8,13 @@ import java.io.IOException;
 
 public class Test {
     private static final String QR_CODE_IMAGE_PATH = "./tmp/QRCode_1.png";
+    private static final String QR_CODE_IMAGE_PATH_2 = "https://gitee.com/YingVickyCao/QRCode_BarCode_Example/raw/main/JavaExample/src/main/resources/qrcode_example.png";
 
     public static void main(String[] args) {
-//        generateQRCodeImage();
-        generateQRCodeImage2();
+        generateQRCodeImage();
+//        generateQRCodeImage2();
 //        generateQRCodeByte();
-//        readQRCode();
+        readQRCode();
     }
 
     private static void generateQRCodeImage() {
@@ -41,9 +42,9 @@ public class Test {
             byte[] bytes = QRCodeUtil.generateQRCodeByte("QR Code Example 苹果梨hellopgfh@123.com", 350, 350);
             FileUtils.saveFile(bytes, QR_CODE_IMAGE_PATH);
         } catch (WriterException e) {
-            System.out.println("Could not generate QR Code, WriterException :: " + e.getMessage());
+            System.out.println("Could not generate QR Code, WriterException:" + e.getMessage());
         } catch (IOException e) {
-            System.out.println("Could not generate QR Code, IOException :: " + e.getMessage());
+            System.out.println("Could not generate QR Code, IOException: " + e.getMessage());
         }
     }
 
